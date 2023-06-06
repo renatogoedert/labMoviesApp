83fffe58a19100527e07f1b40238b72d3893ce22
RGoedert <renato.goedert@gmail.com>
2023-06-06 18:02:06 +0100

    Full review page.

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,9 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes, Link } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
-import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
+import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
+import MovieReviewPage from "./pages/movieReviewPage";
+
 
 const App = () => {
   return (
@@ -21,6 +23,7 @@ const App = () => {
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<Navigate to="/" />} />
+        <Route path="/reviews/:id" element={<MovieReviewPage/>} />
       </Routes>
     </BrowserRouter>
   );
