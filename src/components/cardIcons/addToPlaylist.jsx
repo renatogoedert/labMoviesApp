d2a42e4a849e826caa883f7bbd3a3a114c6be8f6
RGoedert <renato.goedert@gmail.com>
2023-06-09 15:24:57 +0100

    New button for Upcoming movies page.

---
+++ src/components/cardIcons/addToPlaylist.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { MoviesContext } from "../../contexts/moviesContext";
+import IconButton from "@mui/material/IconButton";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+
+const AddToPlaylistIcon = ({ movie }) => {
+  const context = useContext(MoviesContext);
+
+  const onUserSelect = (e) => {
+    e.preventDefault();
+    context.addToFavourites(movie);
+  };
+  return (
+    <IconButton aria-label="add to favorites" onClick={onUserSelect}>
+      <PlaylistAddIcon color="primary" fontSize="large" />
+    </IconButton>
+  );
+};
+
+export default AddToPlaylistIcon;
--- src/pages/upcomingMoviesPage.jsx
+++ src/pages/upcomingMoviesPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from '../components/templateMovieListPage';
 import { getUpcomingMovies } from "../api/tmdb-api";
-import AddToFavouritesIcon from '../components/cardIcons/addToFavourites';
+import AddToPlaylistIcon from '../components/cardIcons/addToPlaylist';
 
 const UpcomingMoviesPage = (props) => {
   const [movies, setMovies] = useState([]);
@@ -27,7 +27,7 @@ const UpcomingMoviesPage = (props) => {
       title="Upcoming Movies"
       movies={movies}
       action={(movie) => {
-        return <AddToFavouritesIcon movie={movie} />
+        return <AddToPlaylistIcon movie={movie} />
       }}
     />
   );
