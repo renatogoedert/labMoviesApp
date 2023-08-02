76ee9dca8ac8027f311c522b7e769bdf123ed4d7
RGoedert <renato.goedert@gmail.com>
2023-08-02 18:14:07 +0100

    Replacing Icon Strategy

--- src/components/cardIcons/addToFavourites.jsx
+++ src/components/cardIcons/addToFavourites.jsx
@@ -1,42 +1,16 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import { MoviesContext } from "../../contexts/moviesContext";
 import IconButton from "@mui/material/IconButton";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { supabase } from "../../api/supabase";
 
 const AddToFavouritesIcon = ({ movie }) => {
-  const [isFavourite, setIsFavourite] = useState(false);
-  const [loading, setLoading] = useState(false);
-  // const toast = useToast();
+  const context = useContext(MoviesContext);
 
-  async function onUserSelect(e) {
+  const onUserSelect = (e) => {
     e.preventDefault();
-    setLoading(true);
-    if (!isFavourite) {
-      const { data, error } = await supabase
-        .from("FavouritesMovies")
-        .insert([{ id:movie.id }]);
-      setIsFavourite(true);
-      setLoading(false);
-    } else {
-      await supabase.from("favouriteMovies").delete().eq([{ movie }]);
-      setIsFavourite(true);
-      setLoading(false);
-    }
-
-    // toast({
-    //   title: error || 'Task added!',
-    //   position: 'top',
-    //   status: error ? 'error' : 'success',
-    //   duration: 2000,
-    //   isClosable: true,
-    // });
-  }
-
-  // const onUserSelect = (e) => {
-  //   e.preventDefault();
-  //   context.addToFavourites(movie);
-  // };
+    context.addToFavourites(movie);
+  };
 
   return (
     <IconButton aria-label="add to favorites" onClick={onUserSelect}>
